cfc7054ac4d478663dc8b6ab73ea699e6ad4e52b
Jonathan Hall <flimzy@flimzy.com>
2023-10-05 14:06:54 +0200

    detect mixed

--- main.go
+++ main.go
@@ -9,19 +9,24 @@ import (
 )
 
 func main() {
-	if err := scanFile(os.Args[1]); err != nil {
+	count, mixed, err := scanFile(os.Args[1])
+	if err != nil {
 		panic(err)
+	}
+	if count > 0 {
+		fmt.Printf("%s: %d naked returns (%d mixed functions)\n", os.Args[1], count, mixed)
 	}
 }
 
-func scanFile(filename string) error {
+func scanFile(filename string) (naked int, mixed int, _ error) {
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, filename, nil, parser.AllErrors)
 	if err != nil {
-		return err
+		return 0, 0, err
 	}
-	ast.Walk(&visitor{fset: fset}, f)
-	return nil
+	v := &visitor{fset: fset}
+	ast.Walk(v, f)
+	return v.found, v.mixed, nil
 }
 
 func foo() (err error) {
@@ -32,42 +37,61 @@ func bar() {}
 
 func baz() error {
 	return func() (err error) {
+		if false {
+			return nil
+		}
 		return
 	}()
 }
 
+type frame struct {
+	hasReturns     bool
+	clothed, naked bool
+}
+
 type visitor struct {
-	fset       *token.FileSet
-	hasReturns []*bool
+	fset  *token.FileSet
+	stack []*frame
+	found int
+	mixed int
 }
 
 func (v *visitor) Visit(node ast.Node) ast.Visitor {
 	if node == nil {
-		if l := len(v.hasReturns); l > 0 {
-			v.hasReturns = v.hasReturns[:l-1]
+		if l := len(v.stack); l > 0 {
+			var p *frame
+			p, v.stack = v.stack[l-1], v.stack[:l-1]
+			if p != nil && p.clothed && p.naked {
+				v.mixed++
+			}
 		}
 		return nil
 	}
 	switch t := node.(type) {
 	case *ast.FuncDecl:
 		hasReturns := t.Type.Results != nil
-		v.hasReturns = append(v.hasReturns, &hasReturns)
+		v.stack = append(v.stack, &frame{hasReturns: hasReturns})
 	case *ast.ReturnStmt:
-		if t.Results == nil && hasReturns(v.hasReturns) {
-			p := v.fset.Position(t.Return)
-			fmt.Printf("%s %d: naked return\n", p.Filename, p.Line)
+		p := v.parent()
+		if p.hasReturns {
+			if t.Results == nil {
+				v.found++
+				p.naked = true
+			} else {
+				p.clothed = true
+			}
 		}
 	default:
-		v.hasReturns = append(v.hasReturns, nil)
+		v.stack = append(v.stack, nil)
 	}
 	return v
 }
 
-func hasReturns(r []*bool) bool {
-	for i := len(r) - 1; i >= 0; i-- {
-		if r[i] != nil {
-			return *r[i]
+func (v *visitor) parent() *frame {
+	for i := len(v.stack) - 1; i >= 0; i-- {
+		if v.stack[i] != nil {
+			return v.stack[i]
 		}
 	}
-	return false
+	return nil
 }
